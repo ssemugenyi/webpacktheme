722e2a3ad690b8b7343b4f940987e1926b396008
ssemugenyi <ssemugenyiisaac2@gmail.com>
2024-01-23 08:55:16 +0300

    theme set up

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Main template file
+ * 
+ * @package Web Pack Theme
+ * 
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Pack Theme</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
